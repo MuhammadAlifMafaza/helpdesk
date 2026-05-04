5347ca1cca36893fceee7a9d50bff01803dc4ef1
Zhuxyd Ryuzaki <ZhuxydRyuzaki@Kings.com>
2026-05-04 07:53:20 +0700

    w

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,17 +9,18 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
+    use HasRoles;
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
     /**
      * Get the attributes that should be cast.
-     *
      * @return array<string, string>
      */
     protected function casts(): array
